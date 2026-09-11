ae21b880c1ae4453b6a4f3e830162fc7d4a67de1
ayomikun286 <edegbaiayomikun@gmail.com>
2026-09-11 14:50:59 +0100

    fix cors

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,9 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173", "https://olivetbhsnosa.org"],
-
+  origin: [
+    "http://localhost:5173",
+    "https://olivetbhsnosa.org",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-
   credentials: true,
-
   allowedHeaders: ["Content-Type", "Authorization"],
 };
